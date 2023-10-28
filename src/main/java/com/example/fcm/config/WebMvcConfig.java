93df7fbf3f80da799bbcd1f41aa29187e2164a1b
hseong3243 <hseong3243@gmail.com>
2023-10-28 20:18:38 +0900

    chore: cors 설정 추가

---
+++ src/main/java/com/example/fcm/config/WebMvcConfig.java
@@ -0,0 +1,20 @@
+package com.example.fcm.config;
+
+import java.util.List;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebMvcConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+            .allowedOrigins("http://localhost:3000")
+            .allowedHeaders("*")
+            .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS")
+            .allowCredentials(true);
+    }
+}
